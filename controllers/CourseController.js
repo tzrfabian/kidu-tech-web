523745c9960b13e0fd1d2eadea70dc6f2e498e48
Fabian Tenzara <fabiantenzara@gmail.com>
2024-08-15 23:11:10 +0700

    adding wip Add Course and adjusting few menu

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -1,5 +1,5 @@
 
-const { Course } = require("../models")
+const { Course, Category } = require("../models")
 
 class CourseController {
     static async getAllCourse(req, res) {
@@ -7,6 +7,16 @@ class CourseController {
             let data = await Course.findAll();
             // res.send(data);
             res.render('CoursePage', {data});
+        } catch (err) {
+            console.log(err);
+            res.send(err);
+        }
+    }
+
+    static async getAddCourse(req, res) {
+        try {
+            let category = await Category.findAll();
+            res.send(category);
         } catch (err) {
             console.log(err);
             res.send(err);
