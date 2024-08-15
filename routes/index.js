180568044b35e3ce117a355dd3c42d1c7f2de625
Fabian Tenzara <fabiantenzara@gmail.com>
2024-08-15 20:18:44 +0700

    adding category page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const CategoryController = require('../controllers/CategoryController');
 const HomePageController = require('../controllers/HomePageController');
 const UserController = require('../controllers/UserController');
 
@@ -12,17 +13,20 @@ router.post('/register', UserController.postRegister);
 router.get('/login', UserController.loginForm);
 router.post('/login', UserController.postLogin)
 
-router.use(function (req, res, next) {
-    console.log(req.session);
-    if(!req.session.userId) {
-        res.redirect('/login?error=please login first!')
-    } else {
-        next();
-    }
+// router.use(function (req, res, next) {
+//     console.log(req.session);
+//     if(!req.session.userId) {
+//         res.redirect('/login?error=please login first!')
+//     } else {
+//         next();
+//     }
 
-    // console.log('Time', Date.now());
-    // next();
-});
+//     // console.log('Time', Date.now());
+//     // next();
+// });
 
-router.get('/', HomePageController.homePage)
+router.get('/', HomePageController.homePage);
+
+router.get('/category', CategoryController.getAllCategory)
+
 module.exports = router;
