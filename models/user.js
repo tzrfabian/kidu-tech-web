2b649d15db48911e1fc67c7ebe0cad9134991389
Fabian Tenzara <fabiantenzara@gmail.com>
2024-08-15 17:26:49 +0700

    adding model association

--- models/user.js
+++ models/user.js
@@ -11,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasOne(models.Profile);
+      User.hasMany(models.UserCourse);
     }
   }
   User.init({
--- models/usercourse.js
+++ models/usercourse.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      UserCourse.belongsTo(models.User);
+      UserCourse.belongsTo(models.Course);
     }
   }
   UserCourse.init({
